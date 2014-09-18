6a1baed56b7e93b65b0e2aa0f4f8ff49c1e78903
daves125125 <daves125125@gmail.com>
2014-09-18 19:03:47 +0100

    Refactoring angular structure

---
+++ LocationListWeb/src/main/webapp/resources/js/summary/summary-controller.js
@@ -0,0 +1,30 @@
+listModule.controller("summaryCtrl", ["$scope", "$route", "$filter", "listService",
+    function ($scope, $route, $filter, listService) {
+
+    $scope.$route = $route;
+
+    $scope.listItems = listService.getList();
+
+    $scope.description = null;
+
+    // List operations
+    $scope.addListItem = function (description) {
+
+        if(description==null){
+            description = "N/A";
+        }
+
+        var dateTime = $filter('date')(new Date, "dd/MM/yyyy HH:mm:ss");
+        var listItem = {desc: description, location: "new loc", timestamp: dateTime};
+
+        listService.addListItem(listItem);
+
+        // Reset Description
+        $scope.description = null;
+    };
+
+    $scope.deleteListItem = function () {
+        listService.deleteListItem();
+    };
+
+}]);
---
+++ LocationListWeb/src/main/webapp/resources/js/summary/summary-factory.js
@@ -0,0 +1,15 @@
+listModule.factory('summaryFactory', function () {
+
+    var listItems = [
+        {'desc': 'do something', 'location': 'stoke', 'timestamp': '17:00'},
+        {'desc': 'do something more', 'location': 'bham', 'timestamp': '18:00'},
+        {'desc': 'do something extra', 'location': 'bmouth', 'timestamp': '12:00'}
+    ];
+
+    return{
+        getList: function () {
+            return listItems;
+        }
+    };
+
+})
