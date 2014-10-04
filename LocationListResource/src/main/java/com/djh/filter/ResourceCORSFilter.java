03df15f53fe81ffb83013076b54d7caad1a0b4e4
daves125125 <daves125125@gmail.com>
2014-10-04 13:21:57 +0100

    commiting refactor

--- LocationListResource/src/main/java/com/djh/filter/ResourceCORSFilter.java
+++ LocationListResource/src/main/java/com/djh/filter/ResourceCORSFilter.java
@@ -16,7 +16,7 @@ public class ResourceCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,accept,content-type");
         chain.doFilter(req, res);
     }
 
