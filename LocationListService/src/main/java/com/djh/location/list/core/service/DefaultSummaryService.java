bc582dd4d410e3d5797f58c797a73ffcefc621bc
daves125125 <daves125125@gmail.com>
2014-10-12 22:11:21 +0100

    commiting refactor

--- LocationListService/src/main/java/com/djh/location/list/core/service/DefaultSummaryService.java
+++ LocationListService/src/main/java/com/djh/location/list/core/service/DefaultSummaryService.java
@@ -1,40 +1,40 @@
-//package com.djh.location.list.core.service;
-//
-//import com.djh.location.list.core.domain.SummaryItem;
-//
-//import java.util.Date;
-//import java.util.List;
-//
-///**
-// * @author David Hancock
-// */
-//public class DefaultSummaryService implements SummaryService {
-//
-//    private LocationListDAO locationListDAO;
-//
-//    @Override
-//    public SummaryItem addSummaryItem(SummaryItem summaryItem) {
-//        summaryItem.setTimestamp(new Date());
-//        return locationListDAO.saveSummaryItem(summaryItem);
-//    }
-//
-//    @Override
-//    public void deleteSummaryItem(String summaryItemId) {
-//        locationListDAO.deleteSummaryItem(summaryItemId);
-//    }
-//
-//    @Override
-//    public List<SummaryItem> findAllSummaryItems() {
-//        List<SummaryItem> summaryItemsList = locationListDAO.findAllSummaryItems();
-//        return summaryItemsList;
-//    }
-//
-//    @Override
-//    public void updateSummaryItem(String summaryItemId, SummaryItem summaryItem) {
-//        // NO-OP TODO
-//    }
-//
-//    public void setLocationListDAO(LocationListDAO locationListDAO) {
-//        this.locationListDAO = locationListDAO;
-//    }
-//}
+package com.djh.location.list.core.service;
+
+import com.djh.location.list.core.domain.SummaryItem;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author David Hancock
+ */
+public class DefaultSummaryService implements SummaryService {
+
+    //private LocationListDAO locationListDAO;
+
+    @Override
+    public SummaryItem addSummaryItem(SummaryItem summaryItem) {
+        summaryItem.setTimestamp(new Date());
+        return null;
+    }
+
+    @Override
+    public void deleteSummaryItem(String summaryItemId) {
+        //locationListDAO.deleteSummaryItem(summaryItemId);
+    }
+
+    @Override
+    public List<SummaryItem> findAllSummaryItems() {
+        //List<SummaryItem> summaryItemsList = locationListDAO.findAllSummaryItems();
+        return null;
+    }
+
+    @Override
+    public void updateSummaryItem(String summaryItemId, SummaryItem summaryItem) {
+        // NO-OP TODO
+    }
+
+    //public void setLocationListDAO(LocationListDAO locationListDAO) {
+       // this.locationListDAO = locationListDAO;
+    //}
+}
